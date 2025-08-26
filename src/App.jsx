4682c9aa249b4cbda53ff5c21ca4c8e20bdad92c
Dev Karan Singh <dev1604karan@gmail.com>
2025-08-26 23:56:32 +0530

    feat: initialize devtinder-web project with React, Vite, and Tailwind CSS
    - Added package.json with dependencies and scripts for development and build.
    - Created main application structure with routing using react-router-dom.
    - Implemented main components: Body, NavBar, Footer, Login, and Profile.
    - Configured Tailwind CSS and DaisyUI for styling.
    - Set up Vite configuration for React and Tailwind CSS integration.

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Body from "./Body";
+import Login from "./Login";
+import Profile from "./Profile";
+
+function App() {
+  return (
+    <>
+      <BrowserRouter basename="/">
+        <Routes>
+          <Route path="/" element={<Body />}>
+            <Route
+              path="/login"
+              element={<Login />}
+            ></Route>
+            <Route
+              path="/profile"
+              element={<Profile />}
+            ></Route>
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
