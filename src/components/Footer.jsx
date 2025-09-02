2423f90e61951ed9ce199f681ce10e29720b2881
Dev Karan Singh <dev1604karan@gmail.com>
2025-09-02 23:46:48 +0530

    - new footer added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,37 @@
 const Footer = () => {
   return (
-    <footer className="footer sm:footer-horizontal footer-center bg-base-300 text-base-content p-4 fixed bottom-0 left-0 right-0 z-10">
-      <aside>
-        <p>
-          Copyright © {new Date().getFullYear()} - All right reserved by ACME
-          Industries Ltd
-        </p>
-      </aside>
+    <footer class="bg-white rounded-lg shadow-sm m-4 dark:bg-gray-800 fixed bottom-0 left-0 right-0 z-10">
+      <div class="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
+        <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">
+          © 2025{" "}
+          <a href="https://flowbite.com/" class="hover:underline">
+            DKSingh
+          </a>
+          . All Rights Reserved.
+        </span>
+        <ul class="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
+          <li>
+            <a href="#" class="hover:underline me-4 md:me-6">
+              About
+            </a>
+          </li>
+          <li>
+            <a href="#" class="hover:underline me-4 md:me-6">
+              Privacy Policy
+            </a>
+          </li>
+          <li>
+            <a href="#" class="hover:underline me-4 md:me-6">
+              Licensing
+            </a>
+          </li>
+          <li>
+            <a href="#" class="hover:underline">
+              Contact
+            </a>
+          </li>
+        </ul>
+      </div>
     </footer>
   );
 };
